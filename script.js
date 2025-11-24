baf7bab7b2aeb0c438041a157b269e4475de27e9
Eddie Gutman <edigutman@gmail.com>
2025-11-24 16:57:32 +0200

    Finish computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hellow world");
+const choice = ['rock', 'paper', 'scissors'];
+const choicesCount = 3;
+
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * choicesCount)
+  return choice[randomNum];
+}
+

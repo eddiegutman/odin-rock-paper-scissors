0546905aa55e895147e84436adc801ebb93a1d3f
Eddie Gutman <edigutman@gmail.com>
2025-11-24 17:02:16 +0200

    Add score variables

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const choice = ['rock', 'paper', 'scissors'];
 const choicesCount = 3;
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * choicesCount)
   return choice[randomNum];
